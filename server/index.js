2250bf77c00ffa0c8c53e82eccf2c45ca8709618
Jash <jashgarala19@gmail.com>
2023-02-17 15:16:10 +0530

    Added cors

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-app.use(cors())
+app.use(
+    cors({
+      origin: "*",
+    })
+  );
+
 app.get('/', (req, res) => {
     res.send('Express JS on Vercel')
 })
